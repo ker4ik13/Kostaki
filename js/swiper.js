d0f6926ebbfc63afe5748888cb944074c952bb67
ker4ik13 <kekustop228@mail.ru>
2024-02-21 19:59:31 +0300

    fix: огромные текстовые правки и по мелочи

--- js/swiper.js
+++ js/swiper.js
@@ -1,26 +1,29 @@
-const scrollButtons = document.querySelectorAll(".scroll_button");
-const scrollTexts = document.querySelectorAll(".scroll_text");
+const scrollButtons = document.querySelectorAll('.scroll_button');
+const scrollTexts = document.querySelectorAll('.scroll_text');
 
-const swiper = new Swiper(".swiper", {
-  loop: true,
+const swiper = new Swiper('.swiper', {
+	loop: true,
+	dynamicMainBullets: 5,
 
-  pagination: {
-    el: ".swiper-pagination",
-  },
+	pagination: {
+		el: '.swiper-pagination',
+		dynamicBullets: true,
+		dynamicMainBullets: 5,
+	},
 
-  navigation: {
-    nextEl: ".my_button_next",
-    prevEl: ".my_button_prev",
-  },
+	navigation: {
+		nextEl: '.my_button_next',
+		prevEl: '.my_button_prev',
+	},
 });
 
 scrollButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    button.parentElement.previousElementSibling.classList.toggle("hidden");
-    if (button.textContent === "continue...") {
-      button.textContent = "roll up";
-    } else {
-      button.textContent = "continue...";
-    }
-  });
+	button.addEventListener('click', () => {
+		button.parentElement.previousElementSibling.classList.toggle('hidden');
+		if (button.textContent === 'continue...') {
+			button.textContent = 'roll up';
+		} else {
+			button.textContent = 'continue...';
+		}
+	});
 });
